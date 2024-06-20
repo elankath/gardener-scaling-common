f8d46c4ff16332d683c8958cc33ed6dfb6aa98c1
Suyash Choudhary <57896905+sssash18@users.noreply.github.com>
2024-06-20 10:22:33 +0530

    Added gardener cluster types

--- types_impl.go
+++ types_impl.go
@@ -4,11 +4,13 @@ import (
 	"crypto/md5"
 	"encoding/binary"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"golang.org/x/exp/maps"
 	"hash"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"slices"
 	"strconv"
 	"strings"
@@ -356,6 +358,8 @@ func CumulatePodRequests(pod *corev1.Pod) corev1.ResourceList {
 	return sumRequests
 }
 
+var ErrKeyNotFound = errors.New("key not found")
+
 func GetZone(labelsMap map[string]any) string {
 	var zone string
 	for _, zoneLabel := range ZoneLabels {
@@ -366,4 +370,68 @@ func GetZone(labelsMap map[string]any) string {
 		}
 	}
 	return zone
+}
+
+func GetInnerMap(parentMap map[string]any, keys ...string) (map[string]any, error) {
+	var mapPath []string
+	childMap := parentMap
+	for _, k := range keys {
+		mapPath = append(mapPath, k)
+		mp, ok := childMap[k]
+		if !ok {
+			return nil, fmt.Errorf("cannot find the child map under mapPath: %s", mapPath)
+		}
+		childMap, ok = mp.(map[string]any)
+		if !ok {
+			return nil, fmt.Errorf("child map is not of type map[string] any under the mapPath: %s", mapPath)
+		}
+	}
+	return childMap, nil
+}
+
+func GetInnerMapValue(parentMap map[string]any, keys ...string) (any, error) {
+	subkeys := keys[:len(keys)-1]
+	childMap, err := GetInnerMap(parentMap, subkeys...)
+	if err != nil {
+		return nil, err
+	}
+	val, ok := childMap[keys[len(keys)-1]]
+	if !ok {
+		return nil, fmt.Errorf("could not find value for keys %q : %w", keys, ErrKeyNotFound)
+	}
+	return val, nil
+}
+
+func AsIntOrString(val any) (target intstr.IntOrString, err error) {
+	switch v := val.(type) {
+	case int64:
+		target = intstr.FromInt32(int32(val.(int64)))
+	case int32:
+		target = intstr.FromInt32(v)
+	case string:
+		return intstr.FromString(v), nil
+	default:
+		err = fmt.Errorf("cannot parse value %q as intstr.IntOrString", val)
+	}
+	return
+}
+
+type ClusterInfo struct {
+	NodeTemplates map[string]NodeTemplate
+	NodeGroups    map[string]NodeGroupInfo
+	WorkerPools   []WorkerPoolInfo
+	CASettings    CASettingsInfo
+}
+
+func (c *ClusterInfo) Init() error {
+	for name, minMax := range c.CASettings.NodeGroupsMinMax {
+		nodeGroup, ok := c.NodeGroups[name]
+		if !ok {
+			return fmt.Errorf("no nodegroup with the name %s", name)
+		}
+		nodeGroup.MinSize = minMax.Min
+		nodeGroup.MaxSize = minMax.Max
+		c.NodeGroups[name] = nodeGroup
+	}
+	return nil
 }
